05f84a019a6c8d34b5ea11ce993ff15f788ef4d1
Leo W <leo.westebbe@gmail.com>
2024-04-19 19:08:51 -0400

    create map.tsx and give everyone coordinates

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,15 +1,24 @@
 import { Box } from "@mantine/core";
-import React from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import contestants from "../assets/contestants.json";
+import cities from "../assets/uscities.json";
+import cancities from "../assets/canadacities.json";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/LWest001/alone-visualized/main/src/assets/north-america.geojson";
+
 
 function Map() {
   return (
-    <Box>
-      <ComposableMap>
-        <Geographies geography={"../assets/north-america.geojson"}>
+    <Box h="100%" bg="red">
+      <ComposableMap
+        projection={"geoAlbers"}
+        projectionConfig={{ scale: 500, center: [0, 50] }}
+      >
+        <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography fill="white" key={geo.rsmKey} geography={geo} />
             ))
           }
         </Geographies>
